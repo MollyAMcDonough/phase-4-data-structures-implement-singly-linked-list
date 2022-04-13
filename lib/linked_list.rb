646f65c6d56b24f3dfbea160fccd8afd069280e4
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-04-13 16:14:53 -0400

    commit tree b15b10a5b9839e7bcdaad113fbfff316738eb6ce

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,73 @@
 require_relative './node'
 
 class LinkedList
+    attr_accessor :head
+
+    def initialize
+        @head = nil
+    end
+
+    def prepend(node)
+        if !@head
+            @head = node
+        else
+            holder = @head
+            @head = node
+            @head.next_node = holder
+        end
+    end
+
+    def append(node)
+        if !@head
+            @head = node
+        else
+            current_node = @head
+            while current_node.next_node != nil
+                current_node = current_node.next_node
+            end
+            current_node.next_node = node
+        end
+    end
+
+    def delete_head
+        if @head
+            @head = @head.next_node
+        end
+    end
+
+    def delete_tail
+        if @head
+            if @head.next_node
+                current_node = @head
+                last_node = @head
+                while current_node.next_node != nil
+                    last_node = current_node
+                    current_node = current_node.next_node
+                end
+                last_node.next_node = nil
+            else
+                @head = nil
+            end
+        end
+    end
+
+    def add_after(idx, node)
+        current_node = @head
+        while idx > 0 && current_node.next_node
+            current_node = current_node.next_node
+            idx -= 1
+        end
+        nextnod = current_node.next_node
+        current_node.next_node = node
+        node.next_node = nextnod
+    end
+
+    def search(val)
+        current_node = @head
+        while current_node.next_node != nil && current_node.data != val
+            current_node = current_node.next_node
+        end
+        current_node
+    end
 
 end
